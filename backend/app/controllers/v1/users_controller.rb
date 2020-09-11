88917fe917d736a4b053ea356b78d90e0615bc29
TerryThreatt <terry.threatt@gmail.com>
2020-09-10 20:27:01 -0500

    stubbed user controller

--- backend/app/controllers/v1/users_controller.rb
+++ backend/app/controllers/v1/users_controller.rb
@@ -1,8 +1,23 @@
 class V1::UsersController < ApplicationController
 
     def create
+        @user = User.find_or_create_by(user_params)
+
+        if @user.valid?
+            render json: @user, status: :created
+        else
+            render json: @user.errors, status: :unprocessable_entity
+        end
     end
 
     def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+    end
+
+    private
+
+    def user_params
+        params.requre(:user).permit(:username, :email, :password)
     end
 end
