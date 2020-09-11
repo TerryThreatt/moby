0d94fd28b87be8b7bd6639bc4627bcd416c73c04
TerryThreatt <terry.threatt@gmail.com>
2020-09-10 19:09:32 -0500

    modified gems and controllers

--- backend/app/controllers/welcome_controller.rb
+++
@@ -1,6 +0,0 @@
-class WelcomeController < ApplicationController
-
-    def home
-        render 'http://127.0.0.1:5500/frontend/index.html'
-    end
-end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# User
+User.create(username: 'terry.threatt', email: 'terry.threatt@gmail.com', password: 'password')
