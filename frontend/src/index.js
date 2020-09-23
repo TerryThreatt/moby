7cd81078024a828a78b8969345c6c442b07512dc
TerryThreatt <terry.threatt@gmail.com>
2020-09-22 23:13:00 -0500

    updated index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,8 @@
 console.log("Testing...")
+
+const BASE_URL = 'http://localhost:5500/';
+const BACKEND_URL = 'http://localhost:3000/';
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+});
