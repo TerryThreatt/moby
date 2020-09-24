1a74b6604c59019d45fa73d1cacedd37b20917ba
TerryThreatt <terry.threatt@gmail.com>
2020-09-23 19:35:33 -0500

    stubbing out home component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,16 @@
-console.log("Testing...")
 
+// routes
 const BASE_URL = 'http://localhost:5500/';
-const BACKEND_URL = 'http://localhost:3000/';
+const ALLDISCUSSIONS_URL = `${BACKEND_URL}/discussions`
+const ALLCOMMENTS_URL = `${BACKEND_URL}/discussions`
+const MYBOOKS_URL = `${BACKEND_URL}/users/${user_id}/books`
+const MYDISCUSSIONS_URL = `${BACKEND_URL}/users/${user_id}/books/${book_id}/discussions`
+const MYCOMMENTS_URL = `${BACKEND_URL}/users/${user_id}/books/${book_id}/discussions/${discussion_id}/comments`
+
+
+// elements
+const home = document.getElementById('logo');
 
 document.addEventListener('DOMContentLoaded', () => {
-    
+
 });
