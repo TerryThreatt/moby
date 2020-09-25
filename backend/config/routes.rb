efb3faeb61e7f2e915d2602225d19cb64a067645
TerryThreatt <terry.threatt@gmail.com>
2020-09-25 10:08:53 -0500

    add javascript components

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get '/discussions', to: 'discussions#public', as: 'public_discussions'
-  get '/discussions/:discussion_id/comments', to: 'discussions#index', as: 'public_comments'
-
+  
   namespace :api do
     namespace :v1 do
       resources :books
