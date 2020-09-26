df08eab5a937b4eebe6edcd36e2cd5091233e82c
TerryThreatt <terry.threatt@gmail.com>
2020-09-26 17:18:17 -0500

    added delete review button

--- frontend/src/components/book.js
+++ frontend/src/components/book.js
@@ -4,7 +4,7 @@ class Book {
     constructor(book) {
         this.id = book.id
         this.title = book.title
-        this.author = book.author 
+        this.author = book.author
         this.genre = book.genre || "n/a"
         this.reviews = book.reviews
 
@@ -16,31 +16,29 @@ class Book {
     renderBook() {
         const books = document.querySelector('#books');
 
-
         // create div
         const bookDiv = document.createElement('div');
-
-        // manipulate div
-        bookDiv.innerHTML = `
-            <h3 class="card-title">${this.title}</h3>
-            <h5 class="card-content">${this.author}</h5>
-            <h5 class="card-content">${this.genre}</h5>
-        `
-
 
         // render review
         if (this.reviews != 0) {
             this.renderBookReviews()
         }
         else {
+            // manipulate div
+            bookDiv.innerHTML = `
+            <h3 class="card-title">${this.title}</h3>
+            <h5 class="card-content">${this.author}</h5>
+            <h5 class="card-content">${this.genre}</h5>
+        `
+            // insert div
+            bookDiv.setAttribute('data.id', `${this.id}`)
+            bookDiv.className = 'card'
+            books.appendChild(bookDiv)
+
+            // new review
             let newReview = new Review(this)
             newReview.renderNewReview()
         }
-
-        // insert div
-        bookDiv.setAttribute('data.id', `${this.id}`)
-        bookDiv.className = 'card'
-        books.appendChild(bookDiv)
 
     }
 
@@ -55,7 +53,7 @@ class Book {
             // manipulate div
             bookDiv.innerHTML = `
                 <h3 class="card-title">${this.title}</h3>
-                <h5>Reviews: </h5>
+                <h5>Review: </h5>
                 <ul>
                     <li class="card-content" data.review.id="${review['id']}">${review['body']}</li>
                 </ul>
@@ -65,6 +63,13 @@ class Book {
             bookDiv.setAttribute('data.id', `${this.id}`)
             bookDiv.className = 'card'
             reviews.appendChild(bookDiv)
+
+            // remove button
+            const button = document.createElement('button')
+            button.setAttribute("data.review.id", review['id'])
+            button.innerText = 'Delete Review'
+            button.addEventListener('click', (e) => Review.removeReview(e))
+            bookDiv.appendChild(button)
             console.log(reviews)
         });
 
--- frontend/src/components/review.js
+++ frontend/src/components/review.js
@@ -10,21 +10,7 @@ class Review {
     }
 
     renderNewReview() {
-
-        // create div
-        const div = document.createElement('div');
-
-        // manipulate div
-        div.innerHTML = `
-            <h3 class = "card-title">${this.book.title}</h3>
-            <textarea class = "card-content">${this.body}</textarea>
-        `
-
-        // insert div
-        div.setAttribute('data.id', `${this.book.id}`)
-        console.log(div)
-        // div.className = 'card-action'
-        // books.appendChild(div)
+        
     }
 
 
