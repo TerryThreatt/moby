1215c11d23bb72de57c9d5d221191ffee657ee55
TerryThreatt <terry.threatt@gmail.com>
2020-09-26 14:27:16 -0500

    fixing renderBook()

--- frontend/src/components/book.js
+++ frontend/src/components/book.js
@@ -6,6 +6,7 @@ class Book {
         this.title = book.title
         this.author = book.author
         this.genre = book.genre
+        this.reviews = book.reviews 
 
         Book.allBooks.push(this)
     }
@@ -20,7 +21,7 @@ class Book {
 
         // manipulate div
         div.innerHTML = `
-            <h3 class="card-content">${this.title}</h3>
+            <h3 class="card-title">${this.title}</h3>
             <h4 class="card-content">${this.author}</h4>
             <h5 class="card-content">${this.genre}</h5>
         `
@@ -30,6 +31,31 @@ class Book {
         div.className = 'card'
         books.appendChild(div)
 
+        // render review
+        if (this.reviews === 0) {
+            let newReview = new Review(this)
+            newReview.renderNewReview()
+        }
+        else {
+            div.innerHTML = `
+                <ul>
+                    <h2>Reviews: </h2>
+                    <li>
+            `
+        }
+
     }
+
+    renderBookReviews() {
+
+
+        div.innerHTML = `
+                <ul>
+                    <h2>Reviews: </h2>
+                    <li>
+            `
+    }
+
+
 
 }
--- frontend/src/components/review.js
+++ frontend/src/components/review.js
@@ -1,13 +1,33 @@
 class Review {
     static allReviews = [];
 
-    constructor(id, book_id, body){
+    constructor(book){
         this.id = id
-        this.book = book_id
-        this.body = body
+        this.book = book
+        this.body = ""
 
         Review.allReviews.push(this)
     }
+
+    renderNewReview() {
+
+        // create div
+        const div = document.createElement('div');
+
+        // manipulate div
+        div.innerHTML = `
+            <h3 class = "card-title">${this.book.title}</h3>
+            <textarea class = "card-content">${this.body}</textarea>
+        `
+
+        // insert div
+        div.setAttribute('data.id', `${this.book.id}`)
+        console.log(div)
+        // div.className = 'card-action'
+        // books.appendChild(div)
+    }
+
+
 
 
 }
